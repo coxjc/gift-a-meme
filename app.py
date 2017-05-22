429021350f7543912fe0e9bf8da64acae45fb0d2
Colby Cox <coxjc@seas.upenn.edu>
2017-05-22 00:59:46 -0400

    SUB: Home Page MSG: Start main page HTML.

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import os
-from flask import Flask 
+from flask import Flask, render_template
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-    return 'Hello, World'
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+    return render_template('hello.html') 
 
 if __name__ == '__main__':
     print(os.environ['APP_SETTINGS'])
