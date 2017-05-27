10972533568ce51f64dc6eaf881de30afff744cc
Colby Cox <coxjc@seas.upenn.edu>
2017-05-27 01:44:40 -0400

    SUB: Uploads to AWS, redirect MSG: Implement upload to AWS, count, redirect, templating, etc. WIP: Security.

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
 import os
-from flask import Flask, render_template
+import requests
+import uuid
+import boto3
+from flask import Flask, render_template, jsonify, request, redirect
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+#end flask setup
+#start aws config
+aws_key = os.environ['aws_key']
+aws_secret = os.environ['aws_secret']
+aws_bucket= os.environ['aws_bucket']
+s3_url = 'https://s3.amazonaws.com/'
+# next_url stores the meme to which the next user will be forwarded
+next_url = 'https://i.ytimg.com/vi/JlhGrcaRTdo/maxresdefault.jpg'
+# counter
+count = 0
+aws = boto3.client(
+    's3',
+    aws_access_key_id=aws_key,
+    aws_secret_access_key=aws_secret
+)
 
 @app.route('/')
-def hello():
-    return render_template('hello.html') 
+def home():
+    return render_template('home.html', count=count)
+
+@app.route('/new', methods=['POST'])
+def new():
+    #set uuid for image key
+    key = str(uuid.uuid4())
+    post = aws.generate_presigned_post(
+            Bucket=aws_bucket, 
+            Key=key,
+            Fields={"acl": "public-read"},
+            Conditions=[{"acl": "public-read"}]
+    )
+    files = {"file": request.files['file']}
+    response = requests.post(post["url"], data=post["fields"], files=files)
+    global next_url, count
+    #update count
+    count += 1
+    #update urls
+    old_url = next_url
+    next_url = s3_url + aws_bucket + '/' + key
+    return render_template('result.html', old_url=old_url, count=count) 
 
 if __name__ == '__main__':
     print(os.environ['APP_SETTINGS'])
